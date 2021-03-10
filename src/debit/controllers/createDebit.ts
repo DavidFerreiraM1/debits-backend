ce9d863d104170d3991c6fc09a312947085864b0
David <davidferreira.m1@outlook.com>
2021-03-10 00:53:27 -0300

    controller para criação de débito criada

--- src/debit/controllers/createDebit.ts
+++ src/debit/controllers/createDebit.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from 'express';
 import { IController } from '../../shared/interfaces/controller.interface';
+import { dataResponse } from '../../shared/utils/data-response';
+import { IDebit } from '../interfaces/debit.interface';
+import { DebitValidation } from '../validations/debit';
 
 class CreateDebit implements IController {
   async run(req: Request, res: Response): Promise<Response> {
     try {
-      const { body } = req;
-      return res.status(204);
+      const body: IDebit = req.body;
+      const { isValid, errors } = new DebitValidation(body);
+
+      if (!isValid) {
+        return res.status(400).send(dataResponse<null>(null, errors));
+      }
+
+      return res.status(201);
+
     } catch (err) {
-      return res.send('ERR0R');
+      return res.status(500).send(err);
     }
   }
 }
