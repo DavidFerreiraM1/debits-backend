00086d439d70ac71d286b19d5096e1370be48640
David Ferreira <david.muniz@armateus.com.br>
2021-03-09 19:52:32 -0500

    chamda de rotas adicionadas

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import router from './router';
 // import router from ''
 
 class App {
@@ -14,7 +15,13 @@ class App {
     this.express.use(express.json());
     this.express.use(morgan(':method :url :status'));
     this.express.use(cors());
+    this.routes();
   }
+
+  private routes(): void {
+    this.express.use('/api', router);
+  }
+
 }
 
 export default new App().express;
