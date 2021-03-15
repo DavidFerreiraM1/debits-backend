55f52f56c8031264bc0d78a88c5392935f4551d4
David <davidferreira.m1@outlook.com>
2021-03-15 00:54:14 -0300

    ajuste para consulta de listagem

--- src/debit/controllers/getAllDebit.ts
+++ src/debit/controllers/getAllDebit.ts
@@ -3,11 +3,17 @@ import { IController } from '../../shared/interfaces/controller.interface';
 import { dataResponse } from '../../shared/utils/data-response';
 import { IDebit } from '../interfaces/debit.interface';
 import getAllRepository from '../repositories/getAllRepository';
+import getDebitByIdRepository from '../repositories/getDebitByIdRespository';
 
 class GetAllDebits implements IController {
   async run(req: Request, res: Response): Promise<Response> {
     try {
-     const result = await getAllRepository.run();
+      const id: any = req.query.id;
+      if (id) {
+        const result = await getDebitByIdRepository.run(parseInt(id));
+        return res.status(200).send(dataResponse<IDebit | null>(result, null));
+      }
+      const result = await getAllRepository.run();
       return res.status(200).send(dataResponse<IDebit[]>(result, null));
 
     } catch (err) {
--- src/debit/repositories/getAllRepository.ts
+++ src/debit/repositories/getAllRepository.ts
@@ -4,7 +4,7 @@ import { IDebit } from '../interfaces/debit.interface';
 class GetAllDebitstRepository {
   async run(): Promise<IDebit[]> {
     try {
-      const result = await Debit.findAll();
+      const result = await Debit.findAll({ where: { active: true } });
       return result;
     } catch (err) {
       throw new Error(err);
