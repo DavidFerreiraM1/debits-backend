2dc8fd41bd646bcdd4ebaf1806321b9f2ca64b24
David <davidferreira.m1@outlook.com>
2021-03-15 00:52:17 -0300

    rotas adicionadas

--- src/debit/routes.ts
+++ src/debit/routes.ts
@@ -1,10 +1,14 @@
 import Express from 'express';
 import createDebit from './controllers/createDebit';
 import getAllDebit from './controllers/getAllDebit';
+import removeDebit from './controllers/removeDebit';
+import updateDebit from './controllers/updateDebit';
 
 const router = Express();
 
 router.post('/', createDebit.run);
 router.get('/', getAllDebit.run);
+router.put('/:id', updateDebit.run);
+router.delete('/:id', removeDebit.run);
 
 export default router;
